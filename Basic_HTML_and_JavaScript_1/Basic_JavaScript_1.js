1d536aee6e0e4771363785c394c4441fdceeab05
wesjiii <153965793+wesjiii@users.noreply.github.com>
2023-12-31 15:17:32 -0800

    Basic_HTML_and_JavaScript_1 folder
    Adding the Basic_HTML_and_JavaScript_1 folder to the HTML & CSS repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang=“en”>
+	<script>
+		function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green") ;
+		document.getElementByID("Irish").innerHTML = result;
+	}
+	</script>
+	<head>
+		<title> write title here </title>
+		<meta charset=“utf-8”
+	</head>
+	<body>
+	</body>
+</html>
